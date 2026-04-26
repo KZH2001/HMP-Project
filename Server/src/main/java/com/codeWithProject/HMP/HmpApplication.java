177b3cfa6872f6536153e37db03964d6dcc4e599
Kyaw Zaw Hein <99558609+KZH2001@users.noreply.github.com>
2026-04-26 23:00:39 +0630

    Initial clean commit

---
+++ Server/src/main/java/com/codeWithProject/HMP/HmpApplication.java
@@ -0,0 +1,13 @@
+package com.codeWithProject.HMP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmpApplication.class, args);
+	}
+
+}
