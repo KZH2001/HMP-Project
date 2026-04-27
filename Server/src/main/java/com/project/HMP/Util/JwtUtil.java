36ffe1a785c37105a7ad2ce3d58951b03a1231f3
Kyaw Zaw Hein <99558609+KZH2001@users.noreply.github.com>
2026-04-28 01:41:03 +0630

    Creating Util for Parsing & Validating JWT

--- Server/src/main/java/com/project/HMP/Util/JwtUtil.java
+++ Server/src/main/java/com/project/HMP/Util/JwtUtil.java
@@ -1,5 +1,6 @@
 package com.project.HMP.Util;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
@@ -11,6 +12,7 @@ import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 @Component
 public class JwtUtil {
@@ -27,6 +29,33 @@ public class JwtUtil {
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 100)) 
                 .signWith(getSigningKey(), SignatureAlgorithm.HS256)
                 .compact();
+    }
+    
+    
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+    	final String userName = extractUserName(token);
+    	return (userName.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+    
+    private Claims extractAllClaims(String token) {
+    	return Jwts.parserBuilder().setSigningKey(getSigningKey()).build().parseClaimsJws(token).getBody();
+    }
+    
+    public String extractUserName(String token) {
+    	return extractClaim(token, Claims::getSubject);
+    }
+    
+    private Date extractExpiration(String token) {
+    	return extractClaim(token, Claims::getExpiration);
+    }
+    
+    private boolean isTokenExpired(String token) {
+    	return extractExpiration(token).before(new Date());
+    }
+    
+    private <T>T extractClaim(String token, Function<Claims,T>claimsResolvers){
+    	final Claims claims = extractAllClaims(token);
+    	return claimsResolvers.apply(claims);
     }
 
     private Key getSigningKey() {
